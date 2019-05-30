15252208d0ca3435066832d30d7f985936167642
Galina Podstrechnaya <galinap684@gmail.com>
2019-05-30 12:27:09 -0400

    retracted coordinate info

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,9 +1,12 @@
 class WeatherController < ApplicationController
     require 'open_weather'
+    require 'json'
 
     def index
        #API_key: `7061597a663b65c93c451d397754049f`
         options = { units: "metric", APPID: '7061597a663b65c93c451d397754049f' } 
         @result = OpenWeather::Current.city("Brooklyn, NY", options)
+        @lat = @result["coord"]["lat"]
+        @lon = @result["coord"]["lon"]
     end
 end
