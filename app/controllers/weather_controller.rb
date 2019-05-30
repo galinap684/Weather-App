481b3f6dde946dd5b1f63bed88a4eefd5f655613
Galina Podstrechnaya <galinap684@gmail.com>
2019-05-30 12:01:35 -0400

    Co-authored-by: Lawrence Dunbar <lawrence@codecode.co>

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,4 +1,9 @@
 class WeatherController < ApplicationController
+    require 'open_weather'
+
     def index
+       #API_key: `7061597a663b65c93c451d397754049f`
+        options = { units: "metric", APPID: '7061597a663b65c93c451d397754049f' } 
+        @result = OpenWeather::Current.city("Brooklyn, NY", options)
     end
 end
