a65c23f2da151fcdb90087cea2bc9e6609cc573b
Galina Podstrechnaya <galinap684@gmail.com>
2019-05-30 13:46:00 -0400

    added 5 day forecast

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -5,12 +5,16 @@ class WeatherController < ApplicationController
     def index
        #API_key: `7061597a663b65c93c451d397754049f`
         options = { units: "metric", APPID: '7061597a663b65c93c451d397754049f' } 
-        @result = OpenWeather::Current.city("Brooklyn, NY", options)
-        @lat = @result["coord"]["lat"]
-        @lon = @result["coord"]["lon"]
-        @icon = @result["weather"][0]["icon"]
-        @description = @result["weather"][0]["description"]
+        @current_weather = OpenWeather::Current.city("Brooklyn, NY", options)
+        @lat = @current_weather["coord"]["lat"]
+        @lon = @current_weather["coord"]["lon"]
+        @icon = @current_weather["weather"][0]["icon"]
+        @description = @current_weather["weather"][0]["description"]
         @description_icon_link = "http://openweathermap.org/img/w/#{@icon}.png"
+
+        options[:cnt] = 5
+        @five_day_forecast = OpenWeather::Forecast.city("Brooklyn, NY", options)
+
 
 
         
